469d43fe1d364bf915714b51a727068ecfeafd8c
Nazar <nazargorodyko@gmail.com>
2024-11-01 23:03:15 +0200

    Solution

---
+++ app/customer.py
@@ -0,0 +1,40 @@
+from dataclasses import dataclass
+import math
+
+from app.car import Car
+from app.shop import Shop
+
+
+@dataclass
+class Customer:
+    name: str
+    products_cart: dict
+    location: list[int]
+    money: int | float
+    car: Car
+
+    def have_money(self) -> str:
+        return f"{self.name} has {self.money} dollars"
+
+    def trip_cost(self, shop: Shop, fuel_price: float) -> int | float:
+        distance = math.sqrt(
+            (self.location[0] - shop.location[0]) ** 2
+            + (self.location[1] - shop.location[1]) ** 2)
+        fuel = distance / 100 * self.car.fuel_consumption
+        return round(fuel * fuel_price * 2, 2)
+
+    def product_cost(self, shop: Shop) -> int | float:
+        if not shop.products:
+            return 0
+        product_price = 0
+        for key, value in self.products_cart.items():
+            if key in shop.products:
+                product_price += value * shop.products[key]
+            else:
+                raise KeyError("There is no required product")
+        return product_price
+
+    def travel_calculation(self, shop: Shop, fuel_price: float) -> int | float:
+        trip_calculate = self.trip_cost(shop, fuel_price)
+        product_calculate = self.product_cost(shop)
+        return trip_calculate + product_calculate
--- app/main.py
+++ app/main.py
@@ -1,3 +1,71 @@
-def shop_trip():
-    # write your code here
-    pass
+import json
+from datetime import datetime
+
+from app.customer import Customer
+from app.car import Car
+from app.shop import Shop
+
+
+def shop_trip() -> None:
+    with open("app/config.json", "r") as f:
+        data = json.load(f)
+
+    fuel_price = data["FUEL_PRICE"]
+    customers = []
+    shops = []
+
+    for customer in data["customers"]:
+        customer_instance = Customer(
+            customer["name"],
+            customer["product_cart"],
+            customer["location"],
+            customer["money"],
+            Car(
+                customer["car"]["brand"],
+                customer["car"]["fuel_consumption"]
+            )
+        )
+        customers.append(customer_instance)
+
+    for shop in data["shops"]:
+        shop_instance = Shop(
+            shop["name"],
+            shop["location"],
+            shop["products"]
+        )
+        shops.append(shop_instance)
+
+    for customer in customers:
+        min_cost = float("inf")
+        choose_shop = ""
+        total_product_price = 0
+        print(customer.have_money())
+
+        for shop in shops:
+            price = customer.travel_calculation(shop, fuel_price)
+            print(f"{customer.name}'s trip to the {shop.name} costs {price}")
+
+            if price < min_cost:
+                min_cost = price
+                choose_shop = shop
+        if min_cost > customer.money:
+            print(f"{customer.name} doesn't have enough "
+                  f"money to make a purchase in any shop")
+            return
+        else:
+            print(f"{customer.name} rides to {choose_shop.name}")
+
+        current_time = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+        print(f"\nDate: {current_time}"
+              f"\nThanks, {customer.name}, for your purchase!"
+              f"\nYou have bought:")
+
+        for key, value in customer.products_cart.items():
+            product_price = value * choose_shop.products[key]
+            print(f"{value} {key}s for {product_price} dollars")
+            total_product_price += product_price
+
+        print(f"Total cost is {total_product_price} dollars\n"
+              f"See you again!\n\n"
+              f"{customer.name} rides home\n"
+              f"{customer.name} now has {customer.money - min_cost} dollars\n")
